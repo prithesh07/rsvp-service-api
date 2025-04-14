22d9b56678ae327bb695bcc043a9a7c13aeba8d3
prithesh07 <prithesh07@gmail.com>
2025-04-13 23:31:11 -0400

    add logging middleware

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import { json } from 'body-parser';
 import setRsvpRoutes from './routes';
 import { setupSwagger } from './config/swagger';
 import { errorHandler } from './middlewares/errorHandler';
+import { loggingMiddleware } from './middlewares/loggingMiddleware';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(json());
+app.use(loggingMiddleware);
 setupSwagger(app);
 setRsvpRoutes(app);
 app.use(errorHandler);
